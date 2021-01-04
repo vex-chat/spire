15370ba89681e9ca73e4db508d4efc148819e281
ExtraHash <ExtraHash@protonmail.com>
2021-01-04 11:32:33 -0700

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
+// TODO: convert to es6 format
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+}
