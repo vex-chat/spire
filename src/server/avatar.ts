f9d16b3c4e69ecd23ebec2f5c093f03731cc735d
ExtraHash <ExtraHash@protonmail.com>
2021-02-01 01:04:31 -0700

    fix linting warning

--- src/server/avatar.ts
+++ src/server/avatar.ts
@@ -8,8 +8,8 @@ import multer from "multer";
 import nacl from "tweetnacl";
 import winston from "winston";
 
-import { Database } from "../Database";
 import { protect } from ".";
+import { Database } from "../Database";
 
 export const getAvatarRouter = (db: Database, log: winston.Logger) => {
     const router = express.Router();
